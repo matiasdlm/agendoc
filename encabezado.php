98b69dc7bd1853413714fed3d3359d4800e71cec
Matias <matias.gdlm@gmail.com>
2018-10-04 18:06:07 -0300

    Se separan las referencias a archivos de estilo y script. Se incluyen en el archivo lib/script.php

--- encabezado.php
+++ encabezado.php
@@ -11,17 +11,7 @@
 
     <title>AgenDoc v0.1</title>
 
-    <!-- Bootstrap core CSS-->
-    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Page level plugin CSS-->
-    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin.css" rel="stylesheet">
+	<?php include_once "lib/script.php"?>
 
   </head>
 
