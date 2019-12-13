836deddb6f4205536f901c5b6fa98bdd6b629222
Matias <matias.gdlm@gmail.com>
2019-12-13 13:33:15 -0300

    Se modifica codigo para que la opcion de busqueda aparezca solo cuando el usuario esta logueado. todavia no funciona

--- encabezado.php
+++ encabezado.php
@@ -29,17 +29,21 @@ session_start();
 			<img src="img/tec.png"> AgenDoc 
 		</a>
 
-      <!-- Navbar Search -->
-      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Alguna búsqueda..." aria-label="Search" aria-describedby="basic-addon2">
-          <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
-              <i class="fas fa-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
+
+      <? php if(isset($_SESSION['usuario'])){ ?>
+          //<!-- Navbar Search -->
+          <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="DNI Paciente..." aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+           </form> 
+
+       <? php } ?>
 
     </nav>
 
