79e971a200838130d9fb6c3e809fa38f434f874f
Matias <matias.gdlm@gmail.com>
2019-12-13 01:29:06 -0300

    Se mejora visualizacion del formulario de registro de usuarios

--- usuarios_form.php
+++ usuarios_form.php
@@ -12,21 +12,21 @@
 		<input type="text" id="apellido" name="apellido"/>
 		<br/>
 		<label for="id_rol"> Rol: </label>
-		<select id="id_rol" name="id_rol" >
+		<select id="id_rol" name="id_rol" style="width:145px">
 		   <option value="1">Secretaria</option>
 		   <option value="2">Especialista</option>
-		</select>
+		</select></br>
 		<label for="usuario">Usuario:</label>
 		<input type="text" id="usuario" name="usuario"/>
 		<br/>
 		<label for="password">Contraseña:</label>
-		<input type="password" id="password" name="password"/>
+		<input style="width:200px" type="password" id="password" name="password"/>
 		<br/>
 		<label for="confirmar_pw">Confirmar Contraseña:</label>
-		<input type="password" id="confirmar_pw" name="confirmar_pw"/>
+		<input style="width:200px" type="password" id="confirmar_pw" name="confirmar_pw"/>
 		<br/><br/>
 		
-		<input type="submit" value="Registrarse"/>
+		<input type="submit" value="Guardar"/>
 	</form> 
 </center>
 </div>
